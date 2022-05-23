fa804565a53943af80a712c55521d08ebbc7ca9b
FuadJamaludin <afuadj90@gmail.com>
2022-05-23 17:44:13 +0200

    created some functions to get electrical and h2 data

--- case_1/case_1_test_53_23.05.py
+++ case_1/case_1_test_53_23.05.py
@@ -5,12 +5,88 @@ from pypsa.linopt import get_var, linexpr, define_constraints
 from geopy.geocoders import Nominatim
 from geopy import distance
 
-csv_folder_name = "C:/Users/work/pypsa/csv_testfiles/1_case_1_test_53"
 
-network = pypsa.Network(csv_folder_name)
+def get_electrical_data(years_elect):
+
+    if years_elect == [2030]:
+        csv_folder_name = "C:/Users/work/pypsa_thesis/data/electrical/1_case_1_2030"
+    elif years_elect == [2040]:
+        csv_folder_name = "C:/Users/work/pypsa_thesis/data/electrical/1_case_1_2040"
+    elif years_elect == [2050]:
+        csv_folder_name = "C:/Users/work/pypsa_thesis/data/electrical/1_case_1_2050"
+
+    return csv_folder_name
+
+
+def get_hydrogen_data(scenario_h2, years_h2):
+
+    if scenario_h2 == 'TN-H2-G':
+        if years_h2 == [2030]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-H2-G/BW_2030.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+        elif years_h2 == [2040]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-H2-G/BW_2040.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+        elif years_h2 == [2050]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-H2-G/BW_2050.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+    elif scenario_h2 == 'TN-PtG-PtL':
+        if years_h2 == [2030]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-PtG-PtL/BW_2030.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+        elif years_h2 == [2040]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-PtG-PtL/BW_2040.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+        elif years_h2 == [2050]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-PtG-PtL/BW_2050.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+    elif scenario_h2 == 'TN-Strom':
+        if years_h2 == [2030]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-Strom/BW_2030.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+        elif years_h2 == [2040]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-Strom/BW_2040.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+        elif years_h2 == [2050]:
+            load_data = pd.read_excel("C:/Users/work/pypsa_thesis/data/hydrogen/TN-Strom/BW_2050.xlsx",
+                                      index_col=0)
+            df_h2_demand = pd.DataFrame(load_data)
+            df_h2_demand.reset_index(inplace=True)
+
+    return df_h2_demand
+
+
 
 years = [2030]
+# years = [2040]
+# years = [2050]
 freq = "24"
+
+network = pypsa.Network(get_electrical_data(years))
 
 snapshots = pd.DatetimeIndex([])
 for year in years:
@@ -27,14 +103,12 @@ network.loads_t.p_set = pd.DataFrame(index=network.snapshots,
                                      columns=network.loads.index,
                                      data=100 * np.random.rand(len(network.snapshots), len(network.loads)))
 
-# network.loads_t.p_set
-
 Nyears = network.snapshot_weightings.objective.sum() / 8760
 Nyears
 
 pmaxpu_generators = network.generators[
     (network.generators['carrier'] == 'Solar') | (network.generators['carrier'] == 'Wind_Offshore') | (
-                network.generators['carrier'] == 'Wind_Onshore')]
+            network.generators['carrier'] == 'Wind_Onshore')]
 
 network.generators_t.p_max_pu = network.generators_t.p_max_pu.reindex(columns=pmaxpu_generators.index)
 
@@ -43,37 +117,28 @@ network.generators_t.p_max_pu.loc[:, pmaxpu_generators.index] = pd.DataFrame(ind
                                                                              data=np.random.rand(len(network.snapshots),
                                                                                                  len(pmaxpu_generators)))
 
-# network.generators_t.p_max_pu
+h2_scenario_demand = "TN-H2-G"
+# h2_scenario_demand = "TN-PtG-PtL"
+# h2_scenario_demand = "TN-PtG-PtL"
 
-excel_BW = pd.read_excel('C:/Users/HP Elitebook 840/Desktop/sources/hydrogen/TN-H2-G/BW_del_special_chars.xlsx',
-                         index_col=0)
-df_BW = pd.DataFrame(excel_BW)
-df_BW.reset_index(inplace=True)
+h2_demand = pd.DataFrame(get_hydrogen_data(h2_scenario_demand, years))
 
-for loc_count in range(len(df_BW['NUTS_NAME'])):
+for loc_count in range(len(h2_demand['NUTS_NAME'])):
     geolocator = Nominatim(user_agent="pycharm")
-    location = geolocator.geocode(df_BW['NUTS_NAME'][loc_count].split(',')[0])
-    df_BW['x'][loc_count] = location.longitude
-    df_BW['y'][loc_count] = location.latitude
-    # print(location.address)
-    # print((location.latitude, location.longitude))
+    location = geolocator.geocode(h2_demand['NUTS_NAME'][loc_count].split(',')[0])
+    h2_demand['x'][loc_count] = location.longitude
+    h2_demand['y'][loc_count] = location.latitude
 
-'''
-possible_city_H2_links = []
-dict_network_dist_store = {}
-for loads_name in list(network.loads.index):
-    dict_network_dist_store['{}'.format(loads_name)] = []
-'''
-
-df_ac_loads_h2_loads_dist = pd.DataFrame(index=network.loads.index, columns=df_BW['NUTS_NAME'])
+df_ac_loads_h2_loads_dist = pd.DataFrame(index=network.loads.index, columns=h2_demand['NUTS_NAME'])
 
 for city_count_x in range(len(network.loads.index)):
-    for city_count_y in range(len(df_BW['NUTS_NAME'])):
-        if network.loads.index[city_count_x] != df_BW['NUTS_NAME'][city_count_y]:
+    for city_count_y in range(len(h2_demand['NUTS_NAME'])):
+        if network.loads.index[city_count_x] != h2_demand['NUTS_NAME'][city_count_y]:
             city_1 = (network.loads['y'][city_count_x], network.loads['x'][city_count_x])
-            city_2 = (df_BW['y'][city_count_y], df_BW['x'][city_count_y])
+            city_2 = (h2_demand['y'][city_count_y], h2_demand['x'][city_count_y])
             dist_city1_city2 = distance.distance(city_1, city_2).km
-            df_ac_loads_h2_loads_dist.at[network.loads.index[city_count_x], df_BW['NUTS_NAME'][city_count_y]] = dist_city1_city2
+            df_ac_loads_h2_loads_dist.at[
+                network.loads.index[city_count_x], h2_demand['NUTS_NAME'][city_count_y]] = dist_city1_city2
 
 ac_loads_H2_links = []
 
@@ -83,8 +148,6 @@ for column_count in df_ac_loads_h2_loads_dist.columns:
             ac_loads_H2_links.append(df_ac_loads_h2_loads_dist.index[distance_count])
 
 ac_loads_H2_links = list(dict.fromkeys(ac_loads_H2_links))
-
-print('hello')
 
 # connect between electrical buses and hydrogen bus via link (as electrolysis unit)
 
@@ -105,12 +168,6 @@ network.madd('Link',
 
 network.add('Store', 'Store Hydrogen', bus='Hydrogen', carrier='Hydrogen', e_nom_extendable=True)
 
-# bus_colors2 = pd.Series("blue",network.buses.index)
-# bus_colors2["Hydrogen"]="green"
-# network.plot(bus_sizes=0.0005, bus_colors=bus_colors2, color_geomap=True)
-# plt.tight_layout()
-
-# case 1
 
 def hydrogen_constraints(n, snapshots):
     electrolysis_index = n.links.query('carrier == "Hydrogen"').index
@@ -119,6 +176,7 @@ def hydrogen_constraints(n, snapshots):
     total_production = 13940000  # 13.94 TWh/a = 13940000 MWh/a of H2 demand in BW
 
     define_constraints(n, lhs, '>=', total_production, 'Link', 'global_hydrogen_production_goal')
+
 
 def extra_functionality(n, snapshots):
     hydrogen_constraints(n, snapshots)
