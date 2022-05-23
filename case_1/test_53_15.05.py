7d9466d5265c9af8a479b4aa98259ea2c1c1b6f0
FuadJamaludin <afuadj90@gmail.com>
2022-05-23 10:00:58 +0200

    minor changes

--- case_1/test_53_15.05.py
+++ case_1/test_53_15.05.py
@@ -16,8 +16,8 @@ freq = "24"
 
 snapshots = pd.DatetimeIndex([])
 for year in years:
-    period = pd.date_range(start ='{}-01-01 00:00'.format(year),
-                           freq ='{}H'.format(freq),
+    period = pd.date_range(start='{}-01-01 00:00'.format(year),
+                           freq='{}H'.format(freq),
                            periods=8760/float(freq))
     snapshots = snapshots.append(period)
 
@@ -25,9 +25,9 @@ network.snapshots = pd.MultiIndex.from_arrays([snapshots.year, snapshots])
 
 network.snapshots
 
-network.loads_t.p_set = pd.DataFrame(index = network.snapshots,
-                                     columns = network.loads.index,
-                                     data = 100*np.random.rand(len(network.snapshots), len(network.loads)))
+network.loads_t.p_set = pd.DataFrame(index=network.snapshots,
+                                     columns=network.loads.index,
+                                     data=100*np.random.rand(len(network.snapshots), len(network.loads)))
 
 #network.loads_t.p_set
 
@@ -38,15 +38,15 @@ pmaxpu_generators = network.generators[(network.generators['carrier'] == 'Solar'
 
 network.generators_t.p_max_pu = network.generators_t.p_max_pu.reindex(columns=pmaxpu_generators.index)
 
-network.generators_t.p_max_pu.loc[:,pmaxpu_generators.index] = pd.DataFrame(index = network.snapshots,
-                                                                          columns = pmaxpu_generators.index,
-                                                                          data = np.random.rand(len(network.snapshots), len(pmaxpu_generators)))
+network.generators_t.p_max_pu.loc[:,pmaxpu_generators.index] = pd.DataFrame(index=network.snapshots,
+                                                                          columns=pmaxpu_generators.index,
+                                                                          data=np.random.rand(len(network.snapshots), len(pmaxpu_generators)))
 
 #network.generators_t.p_max_pu
 
 #connect between electrical buses and hydrogen bus via link (as electrolysis unit)
 
-network.add('Bus', 'Hydrogen', carrier = 'Hydrogen', x = 8.5, y = 49.0)
+network.add('Bus', 'Hydrogen', carrier='Hydrogen', x=8.5, y=49.0)
 
 link_buses = ['Daxlanden_110kV', 'GKMannheim_110kV', 'Leimen_110kV', 'Oberwald_110kV', 'Kuppenheim_110kV', 'Weier_110kV',
              'Lorrach_110kV', 'Kuhmoos_110kV', 'Ravensburg_110kV', 'Leutkirch_110kV', 'Ehingen_110kV', 'Schmiechen_110kV',
@@ -64,11 +64,11 @@ network.madd('Link',
              bus1='Hydrogen',
              efficiency=0.8)
 
-network.add('Store', 'Store Hydrogen', bus = 'Hydrogen', carrier = 'Hydrogen', e_nom_extendable = True)
+network.add('Store', 'Store Hydrogen', bus='Hydrogen', carrier='Hydrogen', e_nom_extendable=True)
 
 bus_colors2 = pd.Series("blue",network.buses.index)
 bus_colors2["Hydrogen"]="green"
-network.plot(bus_sizes = 0.0005, bus_colors = bus_colors2, color_geomap=True)
+network.plot(bus_sizes=0.0005, bus_colors=bus_colors2, color_geomap=True)
 plt.tight_layout()
 
 
