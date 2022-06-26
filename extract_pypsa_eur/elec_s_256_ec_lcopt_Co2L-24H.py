14fa5f86080cc4d08b4488d16ce6906d3467619e
FuadJamaludin <afuadj90@gmail.com>
2022-06-26 22:03:34 +0200

    change source of folder

--- extract_pypsa_eur/elec_s_256_ec_lcopt_Co2L-24H.py
+++ extract_pypsa_eur/elec_s_256_ec_lcopt_Co2L-24H.py
@@ -1,6 +1,6 @@
 import pypsa
 
-network = pypsa.Network("C:/Users/work/pypsa/pypsa-eur-results/elec_s_256_ec_lcopt_Co2L-24H_15.3.22/elec_s_256_ec_lcopt_Co2L-24H.nc")
+network = pypsa.Network("C:/Users/work/pypsa/pypsa-eur_results/elec_s_256_ec_lcopt_Co2L-24H_15.3.22/elec_s_256_ec_lcopt_Co2L-24H.nc")
 
 print('a')
 print('b')
